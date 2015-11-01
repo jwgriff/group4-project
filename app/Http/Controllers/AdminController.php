c0cd5894434543a28535c86a1ebd14cf5db3ef5b
weford <weford@gmail.com>
2015-11-01 13:50:33 -0500

    updated to handle edit of investor, founder models

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Founder;
+use App\Models\Investor;
+use App\Models\Campaign;
+
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -14,4 +18,23 @@ class AdminController extends Controller
         //dd($users);
         return view('admin.home', compact('users'));
     }
+
+
+    public function editFounder($id){
+        $founder = Founder::findOrNew($id);
+        return view('admin.edit', ['founder'=> Founder::findOrNew($id),
+                                    'userType'=> 'Founder']);
+    }
+
+    public function editInvestor($id){
+        //$investor = Investor::findOrNew($id);
+        return view('admin.edit', ['investor'=> Investor::findOrNew($id),
+                                    'userType'=> 'Investor']);
+    }
+    /*
+        public function editAdmin($id){
+            $returnModel = Founder::findOrNew($id);
+            return view('admin.edit', compact('$returnModel'));
+        }
+    */
 }
