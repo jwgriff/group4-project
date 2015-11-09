11b42edfa941ee5ef7cf4bbd55571db9ac155838
weford <weford@gmail.com>
2015-11-09 11:48:19 -0500

    added nav bar to footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -7,3 +7,10 @@
  */
 ?>
 <div id="copyright text-right">&copy; Copyright 2015 ISTM-6213: Group4</div>
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{!! action('PublicController@main') !!}">GWU CroudFunder</a>
+        </div>
+    </div>
+</nav>
