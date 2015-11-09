6cdb2c66a0a3d9c43f0e4f144e2a1af432b90ceb
weford <weford@gmail.com>
2015-11-09 11:35:56 -0500

    updated table structure

--- database/migrations/2015_11_03_214939_create_campaign_investor_ptable.php
+++ database/migrations/2015_11_03_214939_create_campaign_investor_ptable.php
@@ -15,22 +15,27 @@ class CreateCampaignInvestorPtable extends Migration
         Schema::create('campaign_investor', function (Blueprint $table) {
             //IDE Generated
             $table->increments('id');
-            $table->timestamps();
-
-            //Developer Generated
-            $table->integer('campaign_id')->unsigned()->index();
-            $table->foreign('campaign_id')
-                ->references('id')
-                ->on('campaigns');
-
-            $table->integer('investor_id')->unsigned()->index();
-            $table->foreign('investor_id')
-                ->references('id')
-                ->on('investors');
-
             $table->string('invst_label');
             $table->float('invst_amount', 8, 2);
-            //$table->timestamps();
+
+            //Developer Generated
+            $table->unsignedInteger('investor_id')->nullable();
+            $table->foreign('investor_id')
+                ->references('id')
+                ->on('investors')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            //Developer Generated
+            $table->unsignedInteger('campaign_id')->nullable();
+            $table->foreign('campaign_id')
+                ->references('id')
+                ->on('campaigns')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            //IDE Generated
+            $table->timestamps();
 
         });
     }
